11e1e0a9d997892c7a3a639b96da826fb7784ea1
siping <siping@cisco.com>
2022-05-23 21:42:27 +0800

    feat: README.md

---
+++ demo/28_event_basic.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+class CustomEvent extends EventEmitter {}
+const ce = new CustomEvent();
+ce.on('test', () => {
+  console.log('This is a test!');
+});
+setInterval(() => {
+  ce.emit('test');
+}, 500);
