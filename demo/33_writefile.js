ecf8e4feacd8ac595bc6cc3ac354084c25265322
siping <siping@cisco.com>
2022-05-23 23:52:58 +0800

    feat: README.md & demo

--- demo/33_writefile.js
+++ demo/33_writefile.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const content = Buffer.from('This is a test.');
 
 // fs.writeFile('./demo/text', 'This is a test', {
-//     encoding: 'utf8'
+//   encoding: 'utf8'
 // }, err => {
-//     if (err) throw err;
-//     console.log('done!');
+//   if (err) throw err;
+//   console.log('done!');
 // })
 
 fs.writeFile('./demo/text', content, err => {
-    if (err) throw err;
-    console.log('done!');
+  if (err) throw err;
+  console.log('done!');
 })
