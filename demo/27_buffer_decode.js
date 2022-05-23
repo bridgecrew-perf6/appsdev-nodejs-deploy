26c04906bdb649f839c5e80a54648130d30032ab
siping <siping@cisco.com>
2022-05-23 20:23:48 +0800

    feat: README.md

--- demo/27_buffer_decode.js
+++ demo/27_buffer_decode.js
@@ -2,13 +2,13 @@ const stringDecode = require('string_decoder');
 const decoder = new stringDecode.StringDecoder('utf-8');
 const buf = Buffer.from('中文字符串！');
 
-for(let i = 0; i < buf.length; i+=5) {
+for (let i = 0; i < buf.length; i+=5) {
   const b = Buffer.allocUnsafe(5);
   buf.copy(b, 0 ,i);
   console.log(b.toString());
 }
 
-for(let i = 0; i < buf.length; i+=5) {
+for (let i = 0; i < buf.length; i+=5) {
   const b = Buffer.allocUnsafe(5);
   buf.copy(b, 0 ,i);
   console.log(decoder.write(b));
