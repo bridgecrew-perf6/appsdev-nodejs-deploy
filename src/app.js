dd05dc4fbfebce8ca6040b0a0fc8128c1fe9640e
siping <siping@cisco.com>
2022-05-24 19:37:45 +0800

    feat: README.md & src

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const http = require('node:http');
 const conf = require('./config/defaultConfig');
 const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
 const server = http.createServer();
 server.on('request', (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.write('<html>');
-	res.write('<body>');
-	res.write('Hello HTTP !');
-	res.write('</body>');
-	res.write('</html>');
-	res.end();
+	const filePath = path.join(conf.root, req.url);
+	fs.stat(filePath, (err, stats) => {
+		if (err) {
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/plain');
+			res.end(`${filePath} is not a directory or file`);
+			return;
+		}
+		if (stats.isFile()) {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/plain');
+			fs.createReadStream(filePath).pipe(res);
+		} else if (stats.isDirectory()) {
+			fs.readdir(filePath, (err, files) => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/plain');
+				res.end(files.join(','));
+			});
+		}
+	});
 });
 server.listen(conf.port, conf.hostname, () => {
 	console.log(chalk.red('Hello World'));
